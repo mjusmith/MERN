e3575bc86be43a67d7cef10f39b8f39ba6ddbf77
Michael Smith <mjusmith1@gmail.com>
2023-06-04 10:33:19 -0500

    Initial Commit

---
+++ hoist.js
@@ -0,0 +1,113 @@
+// GIVEN
+console.log(example);
+var example = "I'm the example!";
+//AFTER HOISTING BY THE INTERPRETER
+// var example;
+// console.log(example); // logs undefined
+// example = "I'm the example!";
+
+
+1.
+console.log(hello);                                   
+var hello = 'world';                                 
+
+// undefined
+// +
+
+2.
+var needle = 'haystack';
+test();
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+
+// test() undefined
+// magnet
+
+3.
+var brendan = 'super cool';
+function print(){
+    brendan = 'only okay';
+    console.log(brendan);
+}
+console.log(brendan);
+
+// super cool
+// +
+
+4.
+var food = 'chicken';
+console.log(food);
+eat();
+function eat(){
+    food = 'half-chicken';
+    console.log(food);
+    var food = 'gone';
+}
+
+// chicken, half-chicken
+// +
+
+5.
+mean();
+console.log(food);
+var mean = function() {
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
+}
+console.log(food);
+
+// undefined, fish, undefined, undefined
+// mean is not a function
+
+6.
+console.log(genre);
+var genre = "disco";
+rewind();
+function rewind() {
+    genre = "rock";
+    console.log(genre);
+    var genre = "r&b";
+    console.log(genre);
+}
+console.log(genre);
+
+// undefined, rock, r&b, disco
+// +
+
+7.
+dojo = "san jose";
+console.log(dojo);
+learn();
+function learn() {
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
+}
+console.log(dojo);
+
+// undefined, seattle, burbank, san jose
+// san jose, + , + , +
+
+8.
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
+function makeDojo(name, students){
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
+}
+
+// Chicago, 65, true - const error
+// { name: 'Chicago', students: 65, hiring: true }
