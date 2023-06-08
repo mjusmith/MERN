f7ba43dbdf1b338e4695e8829f4c0f7fb2c181f1
Michael Smith <mjusmith1@gmail.com>
2023-06-07 22:21:11 -0500

    useState changes

--- props/props/src/App.js
+++ props/props/src/App.js
@@ -5,13 +5,13 @@ import People from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      <People lastName={"Doe"} firstName="Jane" age="45" hairColor="Black"/>
+      <People lastName={"Doe"} firstName={"Jane"} age={45} hairColor={"Black"}/>
 
-      <People lastName={"Smith"} firstName="John" age="88" hairColor="Brown"/>
+      <People lastName={"Smith"} firstName={"John"} age={88} hairColor={"Brown"}/>
 
-      <People lastName={"Fillmore"} firstName="Millard" age="50" hairColor="Brown"/>
+      <People lastName={"Fillmore"} firstName={"Millard"} age={50} hairColor={"Brown"}/>
 
-      <People lastName={"Smith"} firstName="Maria" age="62" hairColor="Brown"/>
+      <People lastName={"Smith"} firstName={"Maria"} age={62} hairColor={"Brown"}/>
     </div>
   );
 }
--- props/props/src/components/PersonCard.jsx
+++ props/props/src/components/PersonCard.jsx
@@ -1,13 +1,20 @@
 import React, {useState} from 'react'
 
 const People = (props) => {
-    const { lastName, firstName, age, hairColor } = props
+    const { lastName, firstName, hairColor } = props
+
+    const [ages, setAges] = useState(props.age)
+
+    const addAge = () => {
+        setAges(ages + 1)
+    }
 
     return (
         <div>
             <h1>{lastName}, {firstName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {ages}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={addAge}> Birthday button for {firstName} {lastName}.</button>
         </div>
     )
 }
