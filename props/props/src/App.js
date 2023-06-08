d4d9afb76c8243c6b8d22f900f93c80a6fdd56c7
Michael Smith <mjusmith1@gmail.com>
2023-06-07 22:04:13 -0500

    ini.com.

---
+++ props/props/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import People from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <People lastName={"Doe"} firstName="Jane" age="45" hairColor="Black"/>
+
+      <People lastName={"Smith"} firstName="John" age="88" hairColor="Brown"/>
+
+      <People lastName={"Fillmore"} firstName="Millard" age="50" hairColor="Brown"/>
+
+      <People lastName={"Smith"} firstName="Maria" age="62" hairColor="Brown"/>
+    </div>
+  );
+}
+
+export default App;
