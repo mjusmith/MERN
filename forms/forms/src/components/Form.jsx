bbac6c63d59b8e2b0bfb38979a2c2746d3065cde
Michael Smith <mjusmith1@gmail.com>
2023-06-09 10:01:21 -0500

    ini.com.

--- forms/forms/src/components/Form.jsx
+++ forms/forms/src/components/Form.jsx
@@ -1,10 +1,49 @@
 import React, { useState } from 'react'
 
 const Form = () => {
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [email, SetEmail] = useState("")
-    const [password, setPassword] = useState("")
+    const [firstName, setFirstName] = useState("");
+    const [firstNameError, setFirstNameError] = useState("");
+
+    const [lastName, setLastName] = useState("");
+    const [lastNameError, setLastNameError] = useState("");
+
+
+    const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState("");
+
+
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    
+    const [passwordError, setPasswordError] = useState("");
+    
+
+
+
+    const handleFirst = (e) => {
+        setFirstName(e.target.value);
+        e.target.value.length <1 ? setFirstNameError("First name is required.") : e.target.value.length < 2 ? setFirstNameError("First name may not be shorter than 2 characters.") : setFirstNameError("");
+    }
+    const handleLast = (e) => {
+        setLastName(e.target.value);
+        e.target.value.length <1 ? setLastNameError("Last name is required.") : e.target.value.length < 2 ? setLastNameError("Last name may not be shorter than 2 characters.") : setLastNameError("");
+    }
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        e.target.value.length <1 ? setEmailError("Email is required.") : e.target.value.length < 2 ? setEmailError("Email may not be shorter than 2 characters.") : setEmailError("");
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+        e.target.value.length <1 ? setPasswordError("Password is required.") : 
+        e.target.value.length < 8 ? setPasswordError("Password may not be shorter than 8 characters.") : 
+        confirmPassword !== e.target.value ? setPasswordError("Password and Confirm Password do not match.") :
+        setPasswordError("");
+    }
+    const handleConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value);
+        confirmPassword !== password ? setPasswordError("Password and Confirm Password do not match.") : 
+        setPasswordError('');
+    }
 
     let [listOfUsers, setListOfUsers] = useState([])
 
@@ -23,27 +62,47 @@ const Form = () => {
                 <br></br>
                 <div className='form-group'>
                     <label>First Name  :</label>
-                    <input type='text' onChange={ (e) => setFirstName(e.target.value)} className='form-inline'></input>
+                    <input type='text' onChange={ handleFirst } className='form-inline'></input>
+                    {
+                        firstNameError ?
+                        <p>{ firstNameError }</p> :
+                        ''
+                    }
                 </div>
                     <br></br>
                 <div className='form-group'>
                     <label>Last Name  :</label>
-                    <input type='text' onChange={ (e) => setLastName(e.target.value)} className='form-inline'></input>
+                    <input type='text' onChange={ handleLast } className='form-inline'></input>
+                    {
+                        lastNameError ?
+                        <p>{ lastNameError }</p> :
+                        ''
+                    }
                 </div>
                     <br></br>
                 <div className='form-group'>
                     <label>Email  :</label>
-                    <input type='text' onChange={ (e) => SetEmail(e.target.value)} className='form-inline'></input>
+                    <input type='text' onChange={ handleEmail } className='form-inline'></input>
+                    {
+                        emailError ?
+                        <p>{ emailError }</p> :
+                        ''
+                    }
                 </div>
                     <br></br>
                 <div className='form-group'>
                     <label>Password  :</label>
-                    <input type='text' onChange={ (e) => setPassword(e.target.value)} className='form-inline'></input>
+                    <input type='text' name='password' onChange={ handlePassword } className='form-inline'></input>
+                    {
+                        passwordError ?
+                        <p>{ passwordError }</p> :
+                        ''
+                    }
                 </div>
                     <br></br>
                 <div className='form-group'>
                     <label>Confirm Password  :</label>
-                    <input></input>
+                    <input type='text' onChange= { handleConfirmPassword } ></input>
                 </div>
                     <br></br>
                 <input type="submit" value="Add User" className='btn btn-primary mt-3'/>
@@ -59,7 +118,7 @@ const Form = () => {
                                 <p>Last Name: {user.lastName}</p>
                                 <p>Email: {user.email}</p>
                                 <p>Password: {user.password}</p>
-                                <p>Confirm Password: {user.password}</p>
+                                <p>Confirm Password: {confirmPassword}</p>
                                 <br></br>
                             </div>
                         )
