3bcb71b27d13cdb81ee8835e3e6d9e06f15badeb
Michael Smith <mjusmith1@gmail.com>
2023-06-21 11:31:17 -0500

    viewOne completion

--- FullValiDemo/client/src/components/Details.jsx
+++ FullValiDemo/client/src/components/Details.jsx
@@ -8,29 +8,22 @@ const Details = () => {
     const {id} = useParams()
 
     useEffect(() =>{
-        axios.get(`http//localhost:8000/api/products/${id}`)
+        axios.get(`http://localhost:8000/api/products/${id}`)
         .then((res) =>{
-
+            setProduct(res.data.results)
         })
         .catch((err) =>{
             console.log('Zoinks! Error!', err)
         })
     }, [id])
 
-
-
-
-
-
-
-
-
-
-
     return (
-        <h1>
-            Details
-        </h1>
+        <div>
+            <h1>Details</h1>
+            <p>Title: {product.title}</p>
+            <p>Price: {product.price}</p>
+            <p>Description: {product.description}</p>
+        </div>
     )
 }
 
